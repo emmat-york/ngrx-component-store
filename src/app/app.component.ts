8186f5dca465479b1c5537b80fc131bb41204bdd
emmat_york <andrei.filimonchyk@gmail.com>
2024-06-22 19:14:11 +0200

    store: finish version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,48 @@
 import { Component } from '@angular/core';
+import { ExampleState, StoreExample, Initials } from './example.store';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: '<h1>https://github.com/emmat-york/ngrx-component-store</h1>',
+  providers: [StoreExample],
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly latestState$ = this.storeExample.latestState$;
+  readonly vm$ = this.storeExample.vm$;
+  readonly acc$ = this.storeExample.acc$;
+
+  constructor(private storeExample: StoreExample) {}
+
+  updater(name: string): void {
+    this.storeExample.updaterExample(name);
+  }
+
+  effect(name: string): void {
+    this.storeExample.effectExample(name);
+  }
+
+  emptyEffect(): void {
+    this.storeExample.emptyEffectExample();
+  }
+
+  getSnapshot(): ExampleState {
+    return this.storeExample.getSnapshot();
+  }
+
+  getInitials(): Initials {
+    return this.storeExample.getInitials();
+  }
+
+  resetState(): void {
+    this.storeExample.resetState();
+  }
+
+  patchInitials(name: string, sureName: string): void {
+    this.storeExample.patchInitials(name, sureName);
+  }
+
+  patchCarBrand(brand: string): void {
+    this.storeExample.patchCarBrand(brand);
+  }
+}
---
+++ src/app/example.store.ts
@@ -0,0 +1,148 @@
+import { Injectable } from '@angular/core';
+import { ComponentStore } from './component-store/component-store';
+import { delay, finalize, Observable, of, switchMap, tap } from 'rxjs';
+
+export interface Initials {
+  name: string;
+  sureName: string;
+}
+
+export interface Contact {
+  name: string;
+  email: string;
+}
+
+export interface Car {
+  isElectric: boolean;
+  brand: string;
+}
+
+/*
+ * Component store extended service
+ * */
+
+export interface ExampleState {
+  name: string;
+  sureName: string;
+  contacts: Contact[];
+  car: Car;
+  age: number | null;
+  isLoading: boolean;
+}
+
+const INITIAL_STATE: ExampleState = {
+  name: '',
+  sureName: '',
+  contacts: [],
+  car: {
+    brand: '',
+    isElectric: false,
+  },
+  age: null,
+  isLoading: false,
+};
+
+@Injectable()
+export class StoreExample extends ComponentStore<ExampleState> {
+  private readonly name$ = this.select(state => state.name);
+  private readonly sureName$ = this.select(state => state.sureName);
+  private readonly contacts$ = this.select(state => state.contacts);
+  private readonly car$ = this.select(state => state.car);
+  private readonly age$ = this.select(state => state.age);
+
+  readonly latestState$ = this.state$;
+
+  readonly vm$ = this.select({
+    name: this.name$,
+    sureName: this.sureName$,
+    contacts: this.contacts$,
+    car: this.car$,
+    age: this.age$,
+  });
+
+  readonly acc$ = this.select(
+    this.name$,
+    this.sureName$,
+    this.age$,
+    this.car$,
+    (name, sureName, age, car) => {
+      return {
+        fullName: `${name} ${sureName}`,
+        isAllowedToDriveCar: age ? age >= 18 : false,
+        car,
+      };
+    },
+  );
+
+  readonly updaterExample = this.updater((state, sureName: string) => {
+    return {
+      ...state,
+      sureName,
+    };
+  });
+
+  readonly effectExample = this.effect((name$: Observable<string>) => {
+    return name$.pipe(
+      switchMap(() => of({ contacts: [] }).pipe(delay(1000))),
+      tap({
+        next: ({ contacts }) => this.setContacts(contacts),
+        error: error => console.error(error),
+        complete: () => console.log('Effect has been completed.'),
+      }),
+      finalize(() => this.setLoading(false)),
+    );
+  });
+
+  readonly emptyEffectExample = this.effect((voidSource$: Observable<void>) => {
+    return voidSource$.pipe(
+      switchMap(() => of({ description: 'Empty effect example.' }).pipe(delay(1000))),
+      tap({
+        next: data => console.log(data),
+        error: error => console.error(error),
+        complete: () => console.log('Empty effect has been completed.'),
+      }),
+    );
+  });
+
+  constructor() {
+    super(INITIAL_STATE);
+  }
+
+  getSnapshot(): ExampleState {
+    return this.get();
+  }
+
+  getInitials(): Initials {
+    return this.get(state => ({ name: state.name, sureName: state.sureName }));
+  }
+
+  setContacts(contacts: Contact[]): void {
+    this.setState(state => ({ ...state, contacts }));
+  }
+
+  setLoading(isLoading: boolean): void {
+    this.setState(state => ({ ...state, isLoading }));
+  }
+
+  resetState(): void {
+    this.setState({
+      name: '',
+      sureName: '',
+      contacts: [],
+      car: {
+        brand: '',
+        isElectric: false,
+      },
+      isLoading: false,
+      age: null,
+    });
+  }
+
+  patchCarBrand(brand: string): void {
+    this.patchState(state => ({ car: { ...state.car, brand } }));
+  }
+
+  patchInitials(name: string, sureName: string): void {
+    this.patchState({ name, sureName });
+  }
+}
