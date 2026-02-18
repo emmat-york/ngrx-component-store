45597e9709f81f32de3d71f7ffb008bf19081b4d
emmat-york <andrei.filimonchyk@gmail.com>
2026-02-18 20:25:23 +0100

    update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { StoreExample } from './example.store';
-import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss',
-  providers: [StoreExample],
-  imports: [AsyncPipe, NgIf],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  readonly map$ = this.store.map$;
-  readonly vm$ = this.store.vm$;
-
-  constructor(private store: StoreExample) {}
-
-  patchWithFn(): void {
-    this.store.patchWithFn('BMW');
-  }
-
-  patchWithObjects(): void {
-    this.store.patchWithObjects('New name', 'New SureName');
-  }
-
-  resetState(): void {
-    this.store.resetState();
-  }
-
-  effectWithPayload(): void {
-    this.store.effectExample(30);
-  }
-}
+export class AppComponent {}
